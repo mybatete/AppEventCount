20ccbccbd00e8c3bd77875391dfad3f2e1162ae7
Marc Batete <mybatete@lcmail.lcsc.edu>
2019-12-21 18:07:48 -0800

    This app counts and displays the number of time an app goes through each lifecycle.

--- AppEventCount/ViewController.swift
+++ AppEventCount/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var didFinishLaunching: UILabel!
+    var launchCount = 0
+    @IBOutlet weak var willResignActive: UILabel!
+    var ResignActiveCount = 0
+    @IBOutlet weak var willEnterForeground: UILabel!
+    var enterForegroundCount = 0
+    @IBOutlet weak var didEnterBackground: UILabel!
+    var enterBackgroundCount = 0
+    @IBOutlet weak var didBecomeActive: UILabel!
+    var becomeActiveCount = 0
+    @IBOutlet weak var willTerminate: UILabel!
+    var terminatedCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateView()
     }
 
-
+    func updateView(){
+        didEnterBackground.text = "The app has entered the background \(enterBackgroundCount) times"
+        didFinishLaunching.text = "The app has been launched \(launchCount) Times"
+        willResignActive.text = " The app has been deactivated \(ResignActiveCount) times"
+        willEnterForeground.text = "The app has come back to foreground  \(enterForegroundCount) times"
+        didBecomeActive.text = "The app has become active \(becomeActiveCount) times"
+        willTerminate.text = "The app has terminated \(terminatedCount) times"
+    }
 }
+
 
